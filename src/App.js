370b4acaaa5f6a0c88afad8da385661d192610e5
Shoaib Hassan <shoaibh370@gmail.com>
2022-07-06 08:37:56 -0400

    Added breakfast.js file and created it as a child component of App.js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Dinner from './Dinner.js';
+import breakfast from './breakfast';
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
         <h1>Let`s Play the GAME............</h1>
+        <br />
+
         <Dinner DishName="Biryani" SweetDish="Kheer"/>
+        <br />
+        
+        <breakfast />
+        <br />
       </header>
     </div>
   );
---
+++ src/breakfast.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function breakfast(){
+    return(
+        <div>
+            <h4>For breakfast, I mostly eat Anda-Pratha.</h4>
+        </div>
+    )
+}
+
+export default breakfast;
