d4751e44fc93f45e0958a3df530c1fd5e74f1129
Shoaib Hassan <shoaibh370@gmail.com>
2022-07-06 07:42:41 -0400

    Add return() function in Dinner.js

--- src/Dinner.js
+++ src/Dinner.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 function Dinner(props){
+    return(
     <div>
         <h3>It`s time to Eat {props.DishName} with SweetDish{props.SweetDish}</h3>
 
     </div>
+    )
 }
 
 export default Dinner;
