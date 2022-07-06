7cb8ca03f380c6d04ed2d9e4eb154ae0eb71a162
Shoaib Hassan <shoaibh370@gmail.com>
2022-07-06 07:30:35 -0400

    Create Dinner.js File and make this file as a child component of APP.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import React from 'react';
 import './App.css';
-
+import Dinner from './Dinner.js';
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
         <h1>Let`s Play the GAME............</h1>
+        <Dinner DishName="Biryani" SweetDish="Kheer"/>
       </header>
     </div>
   );
---
+++ src/Dinner.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Dinner(props){
+    <div>
+        <h3>It`s time to Eat {props.DishName} with SweetDish{props.SweetDish}</h3>
+
+    </div>
+}
